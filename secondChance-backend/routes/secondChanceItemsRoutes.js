c654287998ac67914fb924cafd1d2c03445dbb10
chandlor martin <chandlor.martin97@gmail.com>
2024-05-12 16:20:42 -0700

    fixed linting issues

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -119,10 +119,10 @@ router.put('/:id', async (req, res, next) => {
 
     // Step 5: task 5 - insert code here
     if (updatepreloveItem) {
-      res.json({ 'uploaded':'success'  })
+      res.json({ "uploaded":"success" })
     } else {
-      res.json({ 'uploaded':'failed' })
-    }  
+      res.json({ "uploaded":"failed" })
+    }
   } catch (e) {
     next(e)
   }
@@ -146,7 +146,7 @@ router.delete('/:id', async (req, res, next) => {
 
     // Step 6: task 4 - insert code here
     await collection.deleteOne({ id: req.params.id })
-    res.json({'deleted':'success'})
+    res.json({ 'deleted':'success' })
   } catch (e) {
     next(e)
   }
